040db493fbd3d71fd8ab8dfdedffb9cb7a48c87d
kevin LEVARLET <raptor501@msn.com>
2023-02-13 15:23:35 +0100

    Premiere fenetre modal crée pour la connexion

---
+++ JS/Javascript.js
@@ -0,0 +1,11 @@
+/*
+const conn = document.getElementById("connex");
+const cl = document.getElementById("bonjour");
+
+
+conn.addEventListener("click", popup);
+
+function popup() {
+    let WindowFeatures = "width=200px, height='200px'";
+    window.open("../Views/Connexion.html", "ConnexionCompte", WindowFeatures);
+}*/
